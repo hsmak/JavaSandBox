dffcf359526b0ed3fad15a9ff6a292ff1ee2cedd
hsmak <husain.alkhamis@gmail.com>
2018-10-11 12:19:59 -0400

    committing changes...

---
+++ Concurrency/src/main/java/others/ThreadInstantiation.java
@@ -0,0 +1,30 @@
+package others;
+
+public class ThreadInstantiation {
+
+    public static void main(String[] args) {
+        viaAnonymousClass();
+        viaLambdaExpression();
+        viaLambdaBlock();
+    }
+
+    private static void viaAnonymousClass() {
+        new Thread() {
+            public void run() {
+                System.out.println("wwww");
+            }
+        }.start();
+    }
+
+    private static void viaLambdaExpression() {
+        new Thread(() -> System.out.println("wwww")).start();
+    }
+
+    private static void viaLambdaBlock() {
+        new Thread(
+                () -> {
+                    System.out.println("wwww");// Notice the semicolon ';'
+                    return;//unnecessary
+                }).start();
+    }
+}
