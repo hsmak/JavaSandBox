7cc40548faf5be6a994acdf062cfada219f1af58
hsmak <3461124+hsmak@users.noreply.github.com>
2019-02-21 13:16:11 -0500

    committing changes..

--- CoreJava/Generics/src/main/java/ExtendingGenericType.java
+++ CoreJava/Generics/src/main/java/ExtendingGenericType.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Created by hsmak on 3/10/15.
@@ -19,7 +20,49 @@ import java.util.List;
  and don’t use a wildcard when you both get and put.
 
  */
+
+class Employee{
+    String firstName;
+    String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
+class Salaried extends Employee{
+    double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
+
 public class ExtendingGenericType {
+
+
+    //ToDo
+     boolean test(List<? extends Employee> l, Predicate<? super Employee> e){
+         return e.test(l.get(0));
+
+    }
+
     public static void main(String[] args){
 
 //        List<Number> numbers = new ArrayList<Integer>();// Invalid
