aac0af2c70cb406f2cb4c5846c56636fc47ffbd7
hsmak <husain.alkhamis@gmail.com>
2020-08-28 00:20:48 -0500

    committing changes...

--- _problems/src/main/java/org/hsmak/letit/ReverseWordsInString.java
+++ _problems/src/main/java/org/hsmak/letit/ReverseWordsInString.java
@@ -57,10 +57,9 @@ public class ReverseWordsInString {
                 return sb.toString();
             }
         },
-        PRESERVE_MULTIPLE_SPACES_IN_PLACE {
+        PRESERVE_MULTIPLE_SPACES_IN_PLACE { // ToDo - optimize it
             @Override
             public String apply(String s) {
-
                 char[] chars = s.toCharArray();
                 int i = 0;
 
@@ -85,19 +84,82 @@ public class ReverseWordsInString {
                 }
                 return String.valueOf(chars);
             }
-        };
 
-        void reverseChars(char[] chars, int beginIndex, int endIndex) {
-            if (beginIndex < 0 || endIndex >= chars.length)
-                throw new IllegalArgumentException(
-                        String.format("Index out of range -> beginIndex{%d} endIndex{%d}", beginIndex, endIndex));
+            private void reverseChars(char[] chars, int beginIndex, int endIndex) {
+                if (beginIndex < 0 || endIndex >= chars.length)
+                    throw new IllegalArgumentException(
+                            String.format("Index out of range -> beginIndex{%d} endIndex{%d}", beginIndex, endIndex));
 
-            for (int lower = beginIndex, upper = endIndex; lower < upper; lower++, upper--) {
-                char temp = chars[lower];
-                chars[lower] = chars[upper];
-                chars[upper] = temp;
+                for (int lower = beginIndex, upper = endIndex; lower < upper; lower++, upper--) {
+                    char temp = chars[lower];
+                    chars[lower] = chars[upper];
+                    chars[upper] = temp;
+                }
+            }
+        },
+        STRATEGY_01 {
+            @Override
+            public String apply(String s) {
+                if (s == null || s.length() == 0) {
+                    return s;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                char[] words = s.toCharArray();
+
+                for (int i = 0; i < words.length; i++) {
+                    if (words[i] == ' ') {
+                        continue;
+                    }
+                    int j = i;
+                    while (j < words.length && words[j] != ' ') {
+                        j++;
+                    }
+                    reverse(words, i, j - 1);
+                    i = j;
+                }
+
+                return sb.append(words).toString();
+            }
+
+            private void reverse(char[] s, int i, int j) {
+                while (i < j) {
+                    char tmp = s[i];
+                    s[i] = s[j];
+                    s[j] = tmp;
+                    i++;
+                    j--;
+                }
+            }
+        },
+        STRATEGY_02 {
+            @Override
+            public String apply(String s) {
+                if (s == null || s.length() == 0) {
+                    return s;
+                }
+                char[] chars = s.toCharArray();
+                int j = 0;
+                while (j < chars.length) {
+                    int i = j;
+                    while (j < chars.length && chars[j] != ' ') {
+                        j++;
+                    }
+                    reverse(chars, i, j - 1);
+                    while (j < chars.length && chars[j] == ' ') {
+                        j++;
+                    }
+                }
+                return new String(chars);
+            }
+
+            private void reverse(char[] chars, int i, int j) {
+                while (i < j) {
+                    char tem = chars[i];
+                    chars[i++] = chars[j];
+                    chars[j--] = tem;
+                }
             }
         }
-
     }
 }
