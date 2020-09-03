487ee046607cae1ec4e54a2ac162be059c99588e
hsmak <husain.alkhamis@gmail.com>
2020-09-03 16:41:22 -0500

    committing changes...

--- core-java/src/main/java/_ocp8/ch10_threads/Ch10AppRunner.java
+++ core-java/src/main/java/_ocp8/ch10_threads/Ch10AppRunner.java
@@ -1,9 +1,7 @@
 package _ocp8.ch10_threads;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
@@ -134,6 +132,8 @@ class Synchronization {
         }
         synchronized (MyInterfaceLock.class) { //ToDo - Will this affect classes implementing this interface? Most likely NO!
         }
+        synchronized (MyClassImpMyI.class) { //ToDo - Will this affect classes implementing this interface? Most likely NO!
+        }
         synchronized (MyClassLock.class) {
         }
     }
@@ -167,26 +167,221 @@ class Synchronization {
     }
 }
 
-class StaticAndNonStaticMethodsAccessingSameField { // They will not block each other!!!
+class StaticAndNonStaticMethodsAccessingSameField { // They will not block each other on the same lock object!!! There will be two locks involved!!
 
 }
 
-class LockingMechanisms {
+class DeadlocksVsLivelocks {
+
+    static Lock lockA_DeadLock = new ReentrantLock();
+    static Lock lockB_DeadLock = new ReentrantLock();
+
+    static Lock lockA_LiveLock = new ReentrantLock();
+    static Lock lockB_LiveLock = new ReentrantLock();
+
+    public static void main(String[] args) throws InterruptedException {
+        printClassNameViaStackWalker(1);
+
+        deadLockThem();
+        liveLockThem();
+
+        System.out.println("Sleeping for 20 sec in the main thread before forcing the JVM to shutdown!");
+        TimeUnit.SECONDS.sleep(20);
+        System.exit(0);
+    }
+
+    private static void liveLockThem() {
+        printMethodNameViaStackWalker(1);
+
+        new Thread(() -> {
+            try {
+                liveLockMeOnLockA();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                liveLockMeOnLockB();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+
+    public static void deadLockThem() {
+        printMethodNameViaStackWalker(1);
+
+        new Thread(() -> {
+            try {
+                deadLockMeOnLockA();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                deadLockMeOnLockB();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static void deadLockMeOnLockB() throws InterruptedException {
+        printMethodNameViaStackWalker(1);
+
+        while (true) {
+            synchronized (lockA_DeadLock) {
+                Thread.sleep(100); // Sleep to encourage the scheduler to place this thread back in the queue and dispatch the other one
+                synchronized (lockB_DeadLock) { // deadlock on lockB
+                    System.out.println("Inside - deadLockMeOnLockB: This might never be reached!");
+                }
+                System.out.println("Outside - 0: This might never be reached!");
+            }
+            System.out.println("Outside - 1: This might never be reached!");
+        }
+    }
+
+    public static void deadLockMeOnLockA() throws InterruptedException {
+        printMethodNameViaStackWalker(1);
+
+        while (true) {
+            synchronized (lockB_DeadLock) {
+                Thread.sleep(100); // Sleep to encourage the scheduler to place this thread back in the queue and dispatch the other one
+                synchronized (lockA_DeadLock) { // deadlock on lockA
+                    System.out.println("Inside - deadLockMeOnLockA: This might never be reached!");
+                }
+                System.out.println("Outside - 0: This might never be reached!");
+            }
+            System.out.println("Outside - 1: This might never be reached!");
+        }
+    }
+
+    public static void liveLockMeOnLockB() throws InterruptedException {
+        printMethodNameViaStackWalker(1);
+
+        while (true) {
+            boolean a = lockA_LiveLock.tryLock();
+            Thread.sleep(100); // Encourage the scheduler to place this thread back in the queue and dispatch the other one
+            boolean b = lockB_LiveLock.tryLock();
+            if (a && b)
+                System.out.println("Inside - liveLockMeOnLockB: This might never be reached! However, because TryLock are nonblocking there's higher chance of success!");
+            if (a)
+                lockA_LiveLock.unlock();
+            if (b)
+                lockB_LiveLock.unlock();
+        }
+
+    }
+
+    public static void liveLockMeOnLockA() throws InterruptedException {
+        printMethodNameViaStackWalker(1);
+
+        while (true) {
+            boolean b = lockB_LiveLock.tryLock();
+            Thread.sleep(100); // Encourage the scheduler to place this thread back in the queue and dispatch the other one
+            boolean a = lockA_LiveLock.tryLock();
+            if (a && b)
+                System.out.println("Inside - liveLockMeOnLockA: This might never be reached! However, because TryLock are nonblocking there's higher chance of success!");
+            if (a)
+                lockA_LiveLock.unlock();
+            if (b)
+                lockB_LiveLock.unlock();
+        }
+    }
+}
+
+class ThreadCommunication {
+    public static void main(String[] args) {
+        printClassNameViaStackWalker(1);
+
+        ProducerConsumer producerConsumer = new ProducerConsumer();
+
+        //Start 2 Producers
+        startNThreads(2, () -> {
+            try {
+                producerConsumer.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // Start 2 Consumers
+        startNThreads(2, () -> {
+            try {
+                producerConsumer.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    static void startNThreads(int n, Runnable r) {
+        Stream.generate(() -> new Thread(r)).limit(n).forEach(Thread::start);
+    }
+
+    static class ProducerConsumer {
+
+        private final static int LIMIT = 10;
+        LinkedList<Integer> list = new LinkedList<>();
+        Object lock = new Object();
+
+        public void produce() throws InterruptedException {
+            int value = 0;
+            Random random = new Random();
+            while (true) {
+                synchronized (lock) {// in order to use lock.wait()/lock.notify() on the lock object, this thread must 1st own the lock on this object via synchronized(lock)
+                    while (list.size() == LIMIT) {
+                        lock.wait();
+                    }
+                    list.add(value++);
+                    lock.notifyAll();
+                }
+                Thread.sleep(random.nextInt(200));
+            }
+        }
+
+        public void consume() throws InterruptedException {
+
+            Random random = new Random();
+
+            while (true) {
+                synchronized (lock) { // in order to use lock.wait()/lock.notify() on the lock object, this thread must 1st own the lock on this object via synchronized(lock)
+
+                    while (list.isEmpty()) {
+                        lock.wait();
+                    }
+
+                    System.out.print("List size is: " + list.size());
+                    int value = list.removeFirst();
+                    System.out.println("; the value is: " + value);
+                    lock.notifyAll();
+                }
+
+                Thread.sleep(random.nextInt(200));
+            }
+        }
+    }
+}
+
+class GranularLockingMechanisms {
     public static void main(String[] args) {
         printClassNameViaStackWalker(1);
     }
 }
 
-class DeadlocksLivelocksStarvationRaceConditions {
-    public static void main(String[] args) {
-        printClassNameViaStackWalker(1);
-    }
-}
 
 class AtomicsAndVolatile {
     public static void main(String[] args) {
         printClassNameViaStackWalker(1);
     }
+}
+
+// Synchronized + Volatile
+class SingletonInstance {
+
 }
 
 /*
